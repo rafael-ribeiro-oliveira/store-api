9ded987f9dbe980ba60f8e4b869392140d988011
Rafael Oliveira <rafael.oliver789@gmail.com>
2020-04-29 16:09:23 -0300

    Testando a primeira classe de dominio  categoria

---
+++ src/main/java/com/meckintech/domain/Categoria.java
@@ -0,0 +1,51 @@
+package com.meckintech.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Categoria implements Serializable {
+
+    private Integer id;
+    private String nome;
+
+    public Categoria() {
+
+    }
+
+    public Categoria(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Categoria setId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Categoria setNome(String nome) {
+        this.nome = nome;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return getId().equals(categoria.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
+
--- src/main/java/com/meckintech/resource/CategoriaResource.java
+++ src/main/java/com/meckintech/resource/CategoriaResource.java
@@ -1,19 +1,34 @@
 package com.meckintech.resource;
 
+import com.meckintech.domain.Categoria;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
-@RequestMapping(value = "/categorias")
+@RequestMapping("/categorias")
 public class CategoriaResource {
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String listar() {
-        return "REST está funcionando";
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Categoria> listar() {
+
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritório");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
+
 
     }
 
 
-    }
+}
 
